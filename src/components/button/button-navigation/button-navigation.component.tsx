ef0c5c77be70c3ddbd23ce518ae58f76e8b9cee5
AleksandarIlicc <aleksandari336@gmail.com>
2023-05-26 16:23:54 +0200

    Add TypeScript in ButtonNavigation component

--- src/components/button/button-navigation/button-navigation.component.tsx
+++ src/components/button/button-navigation/button-navigation.component.tsx
@@ -2,7 +2,17 @@ import { FaBars } from "react-icons/fa";
 
 import "./button-navigation.styles.scss";
 
-const ButtonNavigation = ({ showNav, setShowNav, stickyNav }) => {
+interface ButtonNavigationType {
+  showNav: boolean;
+  setShowNav: React.Dispatch<React.SetStateAction<boolean>>;
+  stickyNav: boolean;
+}
+
+const ButtonNavigation: React.FC<ButtonNavigationType> = ({
+  showNav,
+  setShowNav,
+  stickyNav,
+}): JSX.Element => {
   return (
     <button
       className={stickyNav ? "btn__bars btn__bars--sticky" : "btn__bars"}
